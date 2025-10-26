dfbe82c2b7673f06abd478cdc1825f01810569e4
zaidan2205 <72377693+zaidan2205@users.noreply.github.com>
2025-10-26 16:31:40 +0700

    aa

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,16 +66,16 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            //'host' => env('DB_HOST', '127.0.0.1'),
-           // 'port' => env('DB_PORT', '5432'),
-           // 'database' => env('DB_DATABASE', 'forge'),
-           // 'username' => env('DB_USERNAME', 'forge'),
-          //  'password' => env('DB_PASSWORD', ''),
-         //   'charset' => 'utf8',
-           // 'prefix' => '',
-          //  'prefix_indexes' => true,
-           // 'search_path' => 'public',
-           // 'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
